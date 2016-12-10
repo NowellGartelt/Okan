45220a71ba6b7e8ab1debdd1166d8b68c9fe4697
Naguchi <lucrerakuri@gmail.com>
2016-12-10 18:22:52 +0900

    #16 共通部分をテンプレートファイルとして外出ししたい

--- closet/view/hat.php
+++ closet/view/hat.php
@@ -3,6 +3,6 @@
   <title><?php echo $hat['title']; ?></title>
   <meta charset="UTF-8">
   <meta name="description" content="<?php echo $hat['description']; ?>">
-  <meta name="keywords" content="Fuen-Works">
+  <meta name="keywords" content="<?php echo $hat['keywords']; ?>">
 </head>
 <body>
--- controller/login.php
+++ controller/login.php
@@ -1,7 +1,8 @@
 <?php
 $hat = array(
   'title' => 'Fuen-Works：ログイン',
-  'description' => 'Fuen-Works：ログイン'
+  'description' => 'Fuen-Works全体のログイン画面',
+  'keywords' => 'Fuen-Works',
 );
 include '../closet/view/hat.php';
 include '../view/login.php';
