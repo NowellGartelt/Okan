57ce2c90b68853e55401add9dd12182d8e961ceb
Naguchi <lucrerakuri@gmail.com>
2016-12-12 23:34:41 +0900

    fixed #11 支払った金額に応じて、オカンの反応を変えたい

--- view/registPayResult.php
+++ view/registPayResult.php
@@ -15,7 +15,7 @@
   <center>
   <p>Okan：登録完了</p><br>
   <p><?php echo $payState; ?>で、<?php echo $payName; ?>に<?php echo $payment; ?>円ね？</p>
-  <p>しょうがないわね、今日だけよ</p><br>
+  <p><?php echo $kogoto['message']; ?></p><br>
   <img src="../lib/img/カーチャン.gif">
   <br><br>
   <p></p>
