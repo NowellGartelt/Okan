1ef9d9d9e75cda96dc5a405f1225e5506aa2a7b0
NowellGartelt <future.folder.1117@gmail.com>
2017-04-25 02:38:31 +0900

    リダイレクト先の変更

--- model/tools/judgeIsLogined.php
+++ model/tools/judgeIsLogined.php
@@ -9,7 +9,8 @@ class judgeIsLogined {
         }
         
         if ($loginID == null) {
-            header( 'Location: /Okan/controller/login.php' );
+//            header( 'Location: /Okan/controller/login.php' );
+            header( 'Location: ../controller/login.php' );
             exit();
         }
     }
