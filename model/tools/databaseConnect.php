947d40c7270a19d64ec8736d0767fd3a1f77af46
NowellGartelt <future.folder.1117@gmail.com>
2018-02-01 21:57:14 +0900

    Docコメントの充実化
    databaseConnect.phpのみ

--- model/tools/databaseConnect.php
+++ model/tools/databaseConnect.php
@@ -1,4 +1,17 @@
-<!-- model/tools/databaseConnect.php -->
 <?php
+/**
+ * データベース接続情報クラス
+ * 
+ * データベースへの接続情報を記述するクラス
+ * モデルクラスからincludeで使用される
+ * 
+ * @author NowellGartelt
+ * @access public
+ * @package model/tools
+ * @name databaseConnect
+ * @var mysqli $link
+ * 
+ */
+
 $link = mysqli_connect('localhost','iinchou','meganekko','Okan');
 mysqli_set_charset($link, 'utf8');
